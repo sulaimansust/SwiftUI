25318511f1780f63f3619f871495d968856c51ba
Sulayman Md <sulayman.md@stud.uni-bamberg.de>
2022-12-08 21:56:49 +0100

    Add custom view, combine in vertical and horizontal stack

--- CombineView/CombineView/ContentView.swift
+++ CombineView/CombineView/ContentView.swift
@@ -9,21 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .leading){
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-            Text("Sundarban")
-                .font(.title)
-                .padding(0.0)
-                .foregroundColor(.green)
-            HStack {
-                Text("Khulna District")
-                    .font(.subheadline)
-                Text("Bangaldesh")
-                    .font(.subheadline)
-            }
-        }.padding()
+        VStack {
+            
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading){
+    //            Image(systemName: "globe")
+    //                .imageScale(.large)
+    //                .foregroundColor(.accentColor)
+                Text("Sundarban")
+                    .font(.title)
+                    .padding(0.0)
+                    .foregroundColor(.green)
+                HStack {
+                    Text("Khulna District")
+                    Spacer()
+                    Text("Bangaldesh")
+                }.font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About Sundarban")
+                    .font(.title2)
+                Text("World largest mangrove forest.")
+            }.padding()
+            Spacer()
+        }
     }
 }
 

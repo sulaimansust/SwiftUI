590dff666265b2c6cad8f4cac2896fb0b6f481e8
Sulayman Md <sulayman.md@stud.uni-bamberg.de>
2022-12-08 21:58:22 +0100

    Init project

---
+++ ListAndNavigation/ListAndNavigation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ListAndNavigation
+//
+//  Created by Md Sulayman on 08.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListAndNavigation/ListAndNavigation/ListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAndNavigationApp.swift
+//  ListAndNavigation
+//
+//  Created by Md Sulayman on 08.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4a49ca3fc8a6f99c2b23f6b4771fd9ff5944a517
Sulayman Md <sulayman.md@stud.uni-bamberg.de>
2022-12-10 17:18:06 +0100

    Add navigation and List

--- ListAndNavigation/ListAndNavigation/View/CircleImage.swift
+++ ListAndNavigation/ListAndNavigation/View/CircleImage.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
     var body: some View {
-        Image("turtlerock")
+        image
             .renderingMode(.original)
             .clipShape(Circle())
             .overlay{
@@ -21,6 +22,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
--- ListAndNavigation/ListAndNavigation/View/LandmarkList.swift
+++ ListAndNavigation/ListAndNavigation/View/LandmarkList.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks, id: \.id) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks, id: \.id) { landmark in
+                
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
+
+
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }

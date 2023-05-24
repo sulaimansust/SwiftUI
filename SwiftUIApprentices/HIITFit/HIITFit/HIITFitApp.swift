cfa62aedce3408d4c917918e73986456abafda58
Sulayman Md <sulayman.md@liebherr.com>
2023-05-24 10:59:05 +0200

    HIITFit initial project

---
+++ SwiftUIApprentices/HIITFit/HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Sulayman Md on 24.05.23.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
